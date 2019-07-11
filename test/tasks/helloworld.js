9c1b62afeddf8730fa129dde4d416770f9ade55a
Evgheni.Antropov <evgheniantropov@izyware.com>
2019-07-11 15:59:06 +0000

    hello world sample task

---
+++ test/tasks/helloworld.js
@@ -0,0 +1,10 @@
+var modtask = function(chain) {
+  var taskToRun = chain.get('queryObject');
+  var progress = 1;
+  chain([
+    ['chain.importProcessor', 'apps/tasks/api:task_chain', taskToRun],
+    ['task.progress', progress++],
+    ['set', 'outcome', { success: true, reason : 'Hello World' }]
+  ]);
+}
+ 
