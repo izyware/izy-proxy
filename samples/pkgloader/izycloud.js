70754220ba5906f2dcc55af4d8808f99123c1a68
christoff.dimitar <dimitarchristoff@izyware.com>
2020-01-04 02:56:52 +0000

    pkgloader/izycloud: remove dependency to Kernel http module and use proxy universal http

--- samples/pkgloader/izycloud.js
+++ samples/pkgloader/izycloud.js
@@ -14,43 +14,58 @@ modtask.incrementalLoadPkg = function(pkgName, options, loadpush, okpush, failpu
    if (!modtask.auth) {
      return failpush({ reason: 'pkgloader auth token is not specified. You must configure the pkgloader.' });
    }
-   return modtask.ldmod(Kernel.getModulePath('http')).callXmlHttpObject(modtask,
-     'POST',
-     'https://izyware.com/apigateway/:ui/ide:cloudstorage/api',
-     // POST data
-     JSON.stringify({
-        action: 'incrementalLoadPkg',
-        pkgName: pkgName,
-        // todo, this needs to be passed as HTTP header (cookie, auth header, etc.)
-        auth: modtask.auth,
-        ide: options.ide,
-        loadDeps: options.loadDeps,
-        releaseEnabled: options.releaseEnabled
-     }),
-     'application/x-www-form-urlencoded', // contenttype,
-     null, // auth,
-     // transportsuccessfn,
-     function(serializedJSONResponse, p2) {
-        try {
-           var response = JSON.parse(serializedJSONResponse);
-           if (!response.success) return failpush(response);
-           if (!response.data) return failpush({ reason: 'invalid response from incrementalLoadPkg'});
-           if (response.data.length == 0) return failpush({ reason: 'package not found: "' + pkgName  + '"'});
-           var increments = response.data;
-           for(var i=0; i < increments.length; ++i) {
-              var j = 0;
-              loadpush(increments[i][j++], increments[i][j++], increments[i][j++]);
-           }
-           return okpush();
-        } catch(e) {
-           failpush({ reason: e.message });
-        };
+   return modtask.ldmod('izy-proxy/features/v2/http').universalHTTP().sendRequest({
+     url: 'https://izyware.com/apigateway/:ui/ide:cloudstorage/api',
+     method: 'POST',
+     headers: {
+       'content-type': 'application/x-www-form-urlencoded'
      },
-     // transportfailfn
-     function(outcome) {
-        failpush(outcome);
+     body: JSON.stringify({
+       action: 'incrementalLoadPkg',
+       pkgName: pkgName,
+       // todo, this needs to be passed as HTTP header (cookie, auth header, etc.)
+       auth: modtask.auth,
+       ide: options.ide,
+       loadDeps: options.loadDeps,
+       releaseEnabled: options.releaseEnabled
+     })
+   }, function(_outcome) {
+       if (!_outcome.success) return failpush(_outcome);
+       // Becuase we use JSON/IO it will always be 200 (no 201, etc.)
+       if (_outcome.status != 200) {
+         var outcome = {
+           success: false,
+           reason: _outcome.responseText || _outcome.reason,
+           status: _outcome.status
+         };
+         if (outcome.status == 0 || outcome.reason == '') {
+           outcome.reason = 'Can not establish a network connection to ' + url;
+         }
+         return
+         return failpush(outcome);
+       }
+
+
+       var serializedJSONResponse = _outcome.responseText;
+       try {
+         var response = JSON.parse(serializedJSONResponse);
+         if (!response.success) return failpush(response);
+         if (!response.data) return failpush({reason: 'invalid response from incrementalLoadPkg'});
+         if (response.data.length == 0) return failpush({reason: 'package not found: "' + pkgName + '"'});
+         var increments = response.data;
+         for (var i = 0; i < increments.length; ++i) {
+           var j = 0;
+           loadpush(increments[i][j++], increments[i][j++], increments[i][j++]);
+         }
+         return okpush();
+       } catch (e) {
+         failpush({reason: e.message});
+       }
+       ;
      }
    );
 }
 
 modtask.auth = null;
+
+modtask.__$d = ['izy-proxy/features/v2/http'];
