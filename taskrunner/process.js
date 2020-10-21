b209010121ade89f80a50cda8ef40b0305d53991
Evgheni.Antropov <evgheniantropov@izyware.com>
2020-10-21 01:50:26 +0000

    fix typo and spacing formatting

--- taskrunner/process.js
+++ taskrunner/process.js
@@ -1,8 +1,8 @@
 var modtask = function() {};
 modtask.listen = function(queryObject, cb, context) {
     var config = queryObject.config || {};
-    var apiExecusionContext = config.apiExecusionContext || 'inline';
-    apiExecusionContext = '//' + apiExecusionContext + '/';
+    var apiExecutionContext = config.apiExecutionContext;
+    var fullApiExec = '//' + apiExecutionContext + '/';
     var apiPackageName = config.apiPackageName;
     if (!apiPackageName) return cb({ reason: 'apiPackageName must be specified in taskrunnerProcessor config.', status: 2000001 });
     apiPackageName += ':';
@@ -17,31 +17,37 @@ modtask.listen = function(queryObject, cb, context) {
     var taskToRun = {};
     modtask.doChain([
         verbose ? ['log', 'Listening as ' + runTimeID] : ['nop'],
-        [apiExecusionContext + apiPackageName + 'crud?readList', {
+        [fullApiExec + apiPackageName + 'crud?readList', {
           runTimeID: runTimeID
         }],
         function(chain) {
             var data = chain.get('outcome').data;
             if (data.length == 0) return chain(verbose ? ['log', '[taskrunner] no outstanding tasks'] : ['nop']);
             taskToRun = data[0];
+
+            /* task_chain (i.e. task.progress) needs these values in order to function properly */
             taskToRun.readonly = readonly;
+            taskToRun.apiExecutionContext = apiExecutionContext;
+
             chain.newChainForModule(modtask, function(outcome) {
                 chain([
-                  verbose ? ['log', 'Recording outcome for task'] : ['nop'],
-                  [apiExecusionContext + apiPackageName + 'crud?taskOutcome', {
-                    verbose: verbose,
-                    taskRunId: taskToRun.id,
-                    readonly: readonly,
-                    outcome: {
-                      reason: outcome.reason,
-                      success: outcome.success
-                    }
-                  }],
-                  [apiExecusionContext + apiPackageName + 'run?queueForReRunIfEnabled', {
-                    taskId: taskToRun.taskId,
-                    readonly: readonly
-                  }],
-                  verbose ? ['log', outcome.success ? 'taskoutcome: success' : 'taskoutcome: ' + outcome.reason + ', callstack: ' + outcome.__callstackStr] : ['nop']
+                    verbose ? ['log', 'Recording outcome for task'] : ['nop'],
+                    [fullApiExec + apiPackageName + 'crud?taskOutcome', {
+                        verbose: verbose,
+                        taskRunId: taskToRun.id,
+                        readonly: readonly,
+                        outcome: {
+                            reason: outcome.reason,
+                            success: outcome.success
+                            }
+                    }],
+                    function(chain) {
+                        chain([fullApiExec + apiPackageName + 'run?queueForReRunIfEnabled', {
+                            taskId: taskToRun.taskId,
+                            readonly: readonly
+                        }]);
+                    },
+                    verbose ? ['log', outcome.success ? 'taskoutcome: success' : 'taskoutcome: ' + outcome.reason + ', callstack: ' + outcome.__callstackStr] : ['nop']
                 ]);
             }, {}, [
                 ['chain.importProcessor', apiPackageName + 'task_chain', taskToRun],
