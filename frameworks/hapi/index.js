c6a85604abbff2a3c75ace4691990cf6b6391574
christoff.dimitar <dimitarchristoff@izyware.com>
2021-03-11 02:07:29 +0000

    update framework documentations and samples

---
+++ frameworks/hapi/index.js
@@ -0,0 +1,19 @@
+var modtask = {};
+modtask.handle = function(__moduleSearchPaths) {
+  return function(req, res) {
+    require('../../index').newChain({
+      chainItems: [
+        ['//inline/' + req.path, (req.method.toUpperCase() === 'POST' ? req.body : req.query)]
+      ],
+      __chainProcessorConfig: {
+        __moduleSearchPaths
+      }
+    }, outcome => {
+      delete outcome.__callstack;
+      delete outcome.__callstackStr;
+      res.send(outcome);
+    });
+  };
+};
+
+module.exports = modtask;
